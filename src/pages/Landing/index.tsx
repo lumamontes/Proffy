0261fe58790058c8b4ffbf64e77ae4ccd4d56aef
Luma Montes <lumagoesmontes@gmail.com>
2020-08-08 18:10:22 -0300

    criado estrutura da landing page + estilizaçao

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import logoImg from '../../assets/imagens/logo.svg'
+import landingImg from '../../assets/imagens/landing.svg'
 
-function Landing (){
+import studyIcon from '../../assets/imagens/study.svg'
+import giveClassesIcon from '../../assets/imagens/give-classes.svg'
+import purpleHeartIcon from '../../assets/imagens/purple-heart.svg'
+
+import './style.css';
+
+function Landing(){
     return (
         <div id="page-landing">
-            <div className="container" id="page-landing-content">
-              <div className="logo-container">
-                <img src={logoImg} alt="Proffy"/>
-                <h2>Sua plataforma de estudos online</h2>
-              </div>
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Educar" />
+                    <h2>Sua plataforna de estudos online</h2>
+                </div>
 
-              <img src="" alt="" className="hero-image"/>
+                <img 
+                src={landingImg} 
+                alt="Plataforma de estudos" 
+                className="hero-image"/>
+
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+                    </a>
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas" />
+                        Dar aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Todal de 200 conexões <img src={purpleHeartIcon} alt="Conexões" />
+                </span>
             </div>
         </div>
-
     )
 }
 
