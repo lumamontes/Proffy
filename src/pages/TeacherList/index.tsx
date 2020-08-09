1b56ef201a69961dfa8bb8092f15db4c23bf079c
Luma Montes <lumagoesmontes@gmail.com>
2020-08-08 21:05:03 -0300

    iniciado listagem de professores e finalizado componente da header

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import './styles.css'
+import PageHeader from '../../components/pageHeader';
+
+
+function TeacherList () {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponívels" />
+        </div>
+    )
+}
+
+
+export default TeacherList;
